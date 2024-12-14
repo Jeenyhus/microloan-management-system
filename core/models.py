6cada9f40871306c8a133e2dae87923496c870e4
Dabwitso Maweemba <mweembadabwitso@gmail.com>
2024-12-14 11:43:45 +0200

    models updated with proper permissions

--- core/models.py
+++ core/models.py
@@ -16,6 +16,20 @@ class Company(models.Model):
 class CustomUser(AbstractUser):
     company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name="users", null=True, blank=True)
     is_admin = models.BooleanField(default=False)  # Admins manage the company account
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='customuser_set',
+        blank=True,
+        help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+        verbose_name='groups',
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='customuser_set',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        verbose_name='user permissions',
+    )
 
     def __str__(self):
         return self.username
