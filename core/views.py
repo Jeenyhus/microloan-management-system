32a3fa9d278288288e13acd27cf754ca122e2c60
Dabwitso Maweemba <mweembadabwitso@gmail.com>
2024-12-14 11:33:33 +0200

    Added core viewsets using Django Rest Framework

--- core/views.py
+++ core/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Company, Borrower, Loan, Repayment
+from .serializers import CompanySerializer, BorrowerSerializer, LoanSerializer, RepaymentSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class CompanyViewSet(ModelViewSet):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+    permission_classes = [IsAuthenticated]
+
+class BorrowerViewSet(ModelViewSet):
+    queryset = Borrower.objects.all()
+    serializer_class = BorrowerSerializer
+    permission_classes = [IsAuthenticated]
+
+class LoanViewSet(ModelViewSet):
+    queryset = Loan.objects.all()
+    serializer_class = LoanSerializer
+    permission_classes = [IsAuthenticated]
+
+class RepaymentViewSet(ModelViewSet):
+    queryset = Repayment.objects.all()
+    serializer_class = RepaymentSerializer
+    permission_classes = [IsAuthenticated]
