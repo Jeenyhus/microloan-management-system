79c887cb47ef6bce557b9ada65f8a400128d7402
Dabwitso Maweemba <mweembadabwitso@gmail.com>
2024-12-14 11:46:51 +0200

    registered the models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Company, CustomUser, Borrower, Loan, Repayment
 
 # Register your models here.
+
+admin.site.register(Company)
+admin.site.register(CustomUser)
+admin.site.register(Borrower)
+admin.site.register(Loan)
+admin.site.register(Repayment)
