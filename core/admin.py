9bb51144ff472388d37f6b689c3ab96c42a4a211
Dabwitso Maweemba <mweembadabwitso@gmail.com>
2024-12-18 08:43:37 +0200

    Registered Loan Application and Payment schedule models

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Company, CustomUser, Borrower, Loan, Repayment
+from .models import Company, CustomUser, Borrower, Loan, Repayment, LoanApplication, PaymentSchedule
 
 # Register your models here.
 
@@ -8,3 +8,5 @@ admin.site.register(CustomUser)
 admin.site.register(Borrower)
 admin.site.register(Loan)
 admin.site.register(Repayment)
+admin.site.register(LoanApplication)
+admin.site.register(PaymentSchedule)
