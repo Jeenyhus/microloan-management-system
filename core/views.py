b6a3398baee29101e1972cdad9d8f9d72cd1ea71
Dabwitso Maweemba <mweembadabwitso@gmail.com>
2024-12-18 08:50:22 +0200

    This is the  updated endpoints with the new models added

--- core/views.py
+++ core/views.py
@@ -1,11 +1,14 @@
 from rest_framework.viewsets import ModelViewSet
-from .models import Company, Borrower, Loan, Repayment
-from .serializers import CompanySerializer, BorrowerSerializer, LoanSerializer, RepaymentSerializer
+from .models import Company, Borrower, Loan, Repayment, LoanApplication, PaymentSchedule
+from .serializers import CompanySerializer, BorrowerSerializer, LoanSerializer, RepaymentSerializer, LoanApplicationSerializer, PaymentScheduleSerializer
 from rest_framework.permissions import IsAuthenticated
 from django.shortcuts import render
 
 def home(request):
     return render(request, 'core/home.html')
+
+def api_documentation(request):
+    return render(request, 'core/api_documentation.html')
 
 class CompanyViewSet(ModelViewSet):
     queryset = Company.objects.all()
@@ -26,3 +29,14 @@ class RepaymentViewSet(ModelViewSet):
     queryset = Repayment.objects.all()
     serializer_class = RepaymentSerializer
     permission_classes = [IsAuthenticated]
+
+class LoanApplicationViewSet(ModelViewSet):
+    queryset = LoanApplication.objects.all()
+    serializer_class = LoanApplicationSerializer
+    permission_classes = [IsAuthenticated]
+
+class PaymentScheduleViewSet(ModelViewSet):
+    queryset = PaymentSchedule.objects.all()
+    serializer_class = PaymentScheduleSerializer
+    permission_classes = [IsAuthenticated]
+
